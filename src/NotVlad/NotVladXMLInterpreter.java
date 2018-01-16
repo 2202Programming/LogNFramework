d82002cfd5e9f10f9d7f7f97c022b5f0075bbec0
Unknown <hiddendragon66@gmail.com>
2018-01-15 20:07:17 -0600

    Added NotVladXMLInterpreter class

---
+++ src/NotVlad/NotVladXMLInterpreter.java
@@ -0,0 +1,13 @@
+package NotVlad;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class NotVladXMLInterpreter {
+	
+}
