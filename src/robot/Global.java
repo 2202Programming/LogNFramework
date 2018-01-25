5ad341125c93f94349dc06bf49e210e16b907d13
Billy Huang <hiddendragon66@gmail.com>
2018-01-24 20:25:12 -0600

    Added Starting Position Enumerations to Global

--- src/robot/Global.java
+++ src/robot/Global.java
@@ -8,17 +8,24 @@ import robotDefinitions.ControlBase;
  * A class for global variables that are not part of any specific class
  */
 public class Global {
-	//to get an instance of Sensor or Solenoid Controllers, use SomeController.getInstance()
+	// to get an instance of Sensor or Solenoid Controllers, use
+	// SomeController.getInstance()
 
-	//don't set!!!!
+	// don't set!!!!
 	public static Map<String, IControl> controlObjects;
-	
+
 	public static ControlBase controllers;
-	
-	public enum TargetSide {L, R};
+
+	public enum TargetSide {
+		L, R
+	};
+
+	public enum StartPosition {
+		L, M, R
+	};
 	
 	public static TargetSide ourSwitchPosition;
 	public static TargetSide scalePosition;
 	public static TargetSide opponentSwitchPosition;
-	
+
 }
