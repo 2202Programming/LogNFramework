330dc37019f89e34b28ec7b8f4734feaf3465db7
leinad2018 <leinad2018@gmail.com>
2018-02-03 14:31:12 -0600

    Removed old lift code

--- src/NotVlad/components/Lift.java
+++ src/NotVlad/components/Lift.java
@@ -1,26 +1,18 @@
 package NotVlad.components;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import NotVlad.MiyamotoControl;
-import comms.SmartWriter;
 import physicalOutput.motors.TalonSRXMotor;
 import robot.Global;
 import robot.IControl;
 
 public class Lift extends IControl {
-	private TalonSRX talon;
-	
 	private MiyamotoControl controller;
 	private TalonSRXMotor motor;
 	private int setPosition;
 	
 	public Lift(TalonSRX talon) {
-		this.talon = talon;
-		talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
-		
 		controller = (MiyamotoControl)Global.controllers;
 		talon.setIntegralAccumulator(0.0,0,0);
 		talon.setSelectedSensorPosition(0, 0, 0);
@@ -31,7 +23,7 @@ public class Lift extends IControl {
 		this.setPosition = 0;
 	}
 	
-	private void setLiftPosition(LiftPosition position){
+	public void setLiftPosition(LiftPosition position){
 		setPosition = position.getNumber();
 	}
 	
@@ -66,29 +58,12 @@ public class Lift extends IControl {
 	}
 	
 	public void teleopInit(){
-		talon.set(ControlMode.Position, 0);
-		talon.setIntegralAccumulator(0.0, 0, 0);
-		talon.setSelectedSensorPosition(0, 0, 0);
-//		talon.configForwardSoftLimitEnable(true, 0);
-//		talon.configForwardSoftLimitThreshold(30000, 0);
-//		talon.configReverseSoftLimitEnable(true, 0);
-//		talon.configReverseSoftLimitThreshold(-30000, 0);
-		
 		motor.reset();
 		setLiftPosition(LiftPosition.BOTTOM);
 		motor.set(setPosition);
 	}
 	
 	public void teleopPeriodic(){
-		double position = controller.getLeftJoystickX();
-		position*= 4096*5*3;
-		//position=Math.max(-30000, position);
-		//position=Math.min(30000, position);
-		SmartWriter.putD("Position", position);
-		talon.set(ControlMode.Position, position);
-		SmartWriter.putD("TalonSpeed", talon.getMotorOutputPercent());
-		SmartWriter.putD("TalonEncoder",talon.getSelectedSensorPosition(0));
-		
 		if(controller.raiseLift()){
 			switch(getCurrentPosition()){
 				case BOTTOM:
--- src/NotVlad/components/LiftPosition.java
+++ src/NotVlad/components/LiftPosition.java
@@ -1,7 +1,7 @@
 package NotVlad.components;
 
 public enum LiftPosition {
-	BOTTOM(0),SWITCH(1),SCALE(2),CLIMB(3);
+	BOTTOM(0),SWITCH(10000),SCALE(20000),CLIMB(100000);
 	
 	private final int number;
 	private LiftPosition(int number){
