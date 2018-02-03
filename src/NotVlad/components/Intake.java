fb6a91f1f3d939c5e07b0f36f5c4ad7a5f862e92
Billy Huang <hiddendragon66@gmail.com>
2018-02-02 18:21:29 -0600

    Moved Button Condition in Intake and Outtake to TeleopPeriodic
    Also added a stop method

--- src/NotVlad/components/Intake.java
+++ src/NotVlad/components/Intake.java
@@ -9,44 +9,48 @@ public class Intake extends IControl {
 	private IMotor intakeMotorLeft;
 	private IMotor intakeMotorRight;
 	private MiyamotoControl controller;
-	public Intake(IMotor intakeMotorLeft, IMotor intakeMotorRight){
-		this.intakeMotorLeft=intakeMotorLeft;
-		this.intakeMotorRight=intakeMotorRight;
+
+	public Intake(IMotor intakeMotorLeft, IMotor intakeMotorRight) {
+		this.intakeMotorLeft = intakeMotorLeft;
+		this.intakeMotorRight = intakeMotorRight;
 	}
-	private void init(){
+
+	private void init() {
 		intakeMotorLeft.set(0);
 		intakeMotorRight.set(0);
-		controller=(MiyamotoControl)(Global.controllers);
+		controller = (MiyamotoControl) (Global.controllers);
 	}
-	public void autonomousInit(){
+
+	public void autonomousInit() {
 		init();
 	}
-	public void teleopInit(){
+
+	public void teleopInit() {
 		init();
 	}
-	public void intake(){
-		if(controller.intake()){
-			intakeMotorLeft.set(0.2);
-			intakeMotorRight.set(-0.2);
-		}
-		else{
-			intakeMotorLeft.set(0);
-			intakeMotorRight.set(0);
-		}
+
+	public void intake() {
+		intakeMotorLeft.set(0.2);
+		intakeMotorRight.set(-0.2);
 	}
-	public void outtake(){
-		if(controller.outtake()){
-			intakeMotorLeft.set(-0.2);
-			intakeMotorRight.set(0.2);
-		}
-		else{
-			intakeMotorLeft.set(0);
-			intakeMotorRight.set(0);
-		}
+
+	public void outtake() {
+		intakeMotorLeft.set(-0.2);
+		intakeMotorRight.set(0.2);
 	}
+
+	public void stop() {
+		intakeMotorLeft.set(0);
+		intakeMotorRight.set(0);
+	}
+
 	public void teleopPeriodic() {
-		intake();
-		outtake();
+		if (controller.intake()) {
+			intake();
+		} else if (controller.outtake()) {
+			outtake();
+		} else {
+			stop();
+		}
 	}
-	
 }
