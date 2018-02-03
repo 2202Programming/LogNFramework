a49c3e6904444b996fc45e6de735f511b607a07c
Unknown <beastromulus@gmail.com>
2018-02-03 12:44:39 -0600

    First version using TalonSRX

--- src/NotVlad/components/Lift.java
+++ src/NotVlad/components/Lift.java
@@ -18,15 +18,26 @@ public class Lift extends IControl {
 		talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 		
 		controller = (MiyamotoControl)Global.controllers;
+		talon.setIntegralAccumulator(0.0,0,0);
+		talon.setSelectedSensorPosition(0, 0, 0);
 	}
 	
 	public void teleopInit(){
 		talon.set(ControlMode.Position, 0);
+		talon.setIntegralAccumulator(0.0, 0, 0);
+		talon.setSelectedSensorPosition(0, 0, 0);
+//		talon.configForwardSoftLimitEnable(true, 0);
+//		talon.configForwardSoftLimitThreshold(30000, 0);
+//		talon.configReverseSoftLimitEnable(true, 0);
+//		talon.configReverseSoftLimitThreshold(-30000, 0);
+	
 	}
 	
 	public void teleopPeriodic(){
 		double position = controller.getLeftJoystickX();
-		position*=4096*5;
+		position*= 4096*5*3;
+		//position=Math.max(-30000, position);
+		//position=Math.min(30000, position);
 		SmartWriter.putD("Position", position);
 		talon.set(ControlMode.Position, position);
 		SmartWriter.putD("TalonSpeed", talon.getMotorOutputPercent());
