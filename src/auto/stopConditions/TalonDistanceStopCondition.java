2e58916dd443dd9c6ce3f39c71edddd78c1bc227
Billy Huang <hiddendragon66@gmail.com>
2018-02-12 19:47:04 -0600

    Changed TalonDistanceStopCondition to use TalonSRXMotors

--- src/auto/stopConditions/TalonDistanceStopCondition.java
+++ src/auto/stopConditions/TalonDistanceStopCondition.java
@@ -1,35 +1,36 @@
 package auto.stopConditions;
 
 import java.util.List;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import auto.IStopCondition;
 import comms.SmartWriter;
+import physicalOutput.motors.TalonSRXMotor;
 
 /**
- * A stop condition based on Talon Sensors
+ * A stop condition based on Talon Motors
  *
- * Precondition: The Talons have sensors that are in the correct start position
+ * Precondition: The Talon Motors have sensors that are in the correct start
+ * position
  */
 public class TalonDistanceStopCondition implements IStopCondition {
-	private List<TalonSRX> talons;
+	private List<TalonSRXMotor> talons;
 	private int duration;
 
-	public TalonDistanceStopCondition(List<TalonSRX> talons, int inches) {
+	public TalonDistanceStopCondition(List<TalonSRXMotor> talons, int inches) {
 		this.talons = talons;
 		duration = inches;
 	}
 
 	public void init() {
-		for (TalonSRX t : talons) {
-			t.setIntegralAccumulator(0.0, 0, 0);
-			t.setSelectedSensorPosition(0, 0, 0);
+		for (TalonSRXMotor t : talons) {
+			t.getTalon().setIntegralAccumulator(0.0, 0, 0);
+			t.getTalon().setSelectedSensorPosition(0, 0, 0);
 		}
 	}
 
 	public boolean stopNow() {
 		double sum = 0;
-		for (TalonSRX t : talons) {
-			sum += t.getSelectedSensorPosition(0);
+		for (TalonSRXMotor t : talons) {
+			sum += t.getTalon().getSelectedSensorPosition(0);
 		}
 		SmartWriter.putD("AUTO - Talon AVG Encoder Count", sum / talons.size());
 		return (sum / talons.size()) > duration;
