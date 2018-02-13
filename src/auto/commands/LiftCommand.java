63a484ada5e0faa6dc71f7d61b9fc7b100704a51
Billy Huang <hiddendragon66@gmail.com>
2018-02-12 19:01:39 -0600

    Added LIft Command

---
+++ src/auto/commands/LiftCommand.java
@@ -0,0 +1,51 @@
+package auto.commands;
+
+import NotVlad.components.Lift;
+import NotVlad.components.LiftPosition;
+import auto.ICommand;
+import auto.IStopCondition;
+import robot.Global;
+
+public class LiftCommand implements ICommand {
+	private IStopCondition stopCondition;
+	private Lift lift;
+	private int finalPosition;
+
+	/**
+	 * Lifts the lifter to a set position
+	 * 
+	 * @param finalPosition
+	 *            The final position the lift should be in encoder counts
+	 * @param stop
+	 *            The stop condition
+	 */
+
+	public LiftCommand(int finalPosition, IStopCondition stop) {
+		stopCondition = stop;
+		this.finalPosition = finalPosition;
+	}
+
+	public LiftCommand(LiftPosition finalPosition, IStopCondition stop) {
+		stopCondition = stop;
+		this.finalPosition = finalPosition.getNumber();
+	}
+
+	public void init() {
+		stopCondition.init();
+		lift = (Lift) Global.controlObjects.get("LIFT");
+	}
+
+	public boolean run() {
+		if (lift == null) {
+			init();
+		}
+
+		lift.setLiftPosition(finalPosition);
+
+		return stopCondition.stopNow();
+	}
+
+	public void stop() {
+		lift.setLiftPosition(finalPosition);
+	}
+}
