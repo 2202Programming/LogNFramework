e97cfb783926cd3ca40a5812aade2e9cfc307d22
Billy Huang <hiddendragon66@gmail.com>
2018-02-12 21:23:45 -0600

    Added Sysout of the Commands in the Generated CommandList

--- src/NotVlad/NotVladXMLInterpreterTester.java
+++ src/NotVlad/NotVladXMLInterpreterTester.java
@@ -2,16 +2,25 @@ package NotVlad;
 
 import java.io.File;
 
+import auto.CommandList;
+import auto.ICommand;
+
 public class NotVladXMLInterpreterTester {
 	public static void main(String[] args) {
+		CommandList test = new CommandList();
 		long startParse = System.nanoTime();
 		try {
 			NotVladXMLInterpreter interp = new NotVladXMLInterpreter(new File("Paths.xml"));
-			interp.getPathList("11");
+			test = interp.getPathList("L3-1");
 		} catch (NullPointerException e) {
 			System.out.println("OOF");
 		}
 		long endParse = System.nanoTime();
 		System.out.println((endParse - startParse) / 1000000 + " Millisecond runtime");
+		System.out.println("Command List: ");
+		for (int i = 0; i < test.size(); i++) {
+			ICommand cur = test.getCommand(i);
+			System.out.println(cur);
+		}
 	}
 }
