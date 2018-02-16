fb7ad826ec08c44a72d5fa99901732df37e50cd9
leinad2018 <leinad2018@gmail.com>
2018-02-16 17:19:10 -0600

    edited enum

--- src/NotVlad/components/LiftPosition.java
+++ src/NotVlad/components/LiftPosition.java
@@ -1,7 +1,7 @@
 package NotVlad.components;
 
 public enum LiftPosition {
-	BOTTOM(0),SWITCH(10000),SCALE(25000),CLIMB(27000),MAX(30000);
+	BOTTOM(0),SWITCH(10000),SCALE(25000),CLIMB(12800),MAX(30000);
 	
 	private final int number;
 	private LiftPosition(int number){
