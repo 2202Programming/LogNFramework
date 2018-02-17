fd25f39ac9a37dfae3abaa74412809bf231e463b
Billy Huang <hiddendragon66@gmail.com>
2018-02-16 18:11:24 -0600

    Made TurnController a IControl Object

--- src/physicalOutput/TurnController.java
+++ src/physicalOutput/TurnController.java
@@ -2,15 +2,19 @@ package physicalOutput;
 
 import edu.wpi.first.wpilibj.PIDOutput;
 import physicalOutput.motors.IMotor;
+import robot.IControl;
 
-public class TurnController implements PIDOutput {
+public class TurnController extends IControl implements PIDOutput {
 	IMotor left;
 	IMotor right;
 
 	/**
 	 * Provides an interface for PIDController to turn with motors
-	 * @param leftMotor The left IMotor
-	 * @param rightMotor The right IMotor
+	 * 
+	 * @param leftMotor
+	 *            The left IMotor
+	 * @param rightMotor
+	 *            The right IMotor
 	 */
 	public TurnController(IMotor leftMotor, IMotor rightMotor) {
 		left = leftMotor;
