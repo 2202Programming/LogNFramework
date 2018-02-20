bb2517b95ca07264713981ac6577c833291c035a
leinad2018 <leinad2018@gmail.com>
2018-02-20 15:47:02 -0600

    Simple safe lift works

--- src/NotVlad/AutomationController.java
+++ src/NotVlad/AutomationController.java
@@ -14,24 +14,17 @@ public class AutomationController extends IControl{
 	private Lift lift;
 	private MotionProfiler profiler;
 	private double[] tiltAngles;
-	private LiftPosition[] positions;
 	
 	public AutomationController(){
 		gyro = (AHRS)SensorController.getInstance().getSensor("NAVX");
 		lift = (Lift)Global.controlObjects.get("LIFT");
 		profiler = (MotionProfiler)Global.controlObjects.get("PROFILER");
 		tiltAngles = new double[5];
-		tiltAngles[0] = 0;
-		tiltAngles[1] = 5;
-		tiltAngles[2] = 10;
-		tiltAngles[3] = 15;
-		tiltAngles[4] = 20;
-		positions = new LiftPosition[5];
-		positions[0] = LiftPosition.BOTTOM;
-		positions[1] = LiftPosition.SWITCH;
-		positions[2] = LiftPosition.CLIMB;
-		positions[3] = LiftPosition.LOWSCALE;
-		positions[4] = LiftPosition.HIGHSCALE;
+		tiltAngles[0] = 10;
+		tiltAngles[1] = 20;
+		tiltAngles[2] = 25;
+		tiltAngles[3] = 30;
+		tiltAngles[4] = 45;
 	}
 	
 	/**
@@ -64,13 +57,18 @@ public class AutomationController extends IControl{
 	
 	private void keepLiftSafe(){
 		double tiltAngle = gyro.getPitch();
-		int tiltSeverity = 0;
-		for(int i = 0; i < tiltAngles.length; i++){
-			if(tiltAngle > tiltAngles[i]){
-				tiltSeverity = i;
-			}
+		int liftSeverity = getLiftSeverity();
+		if(tiltAngle > tiltAngles[liftSeverity]){
+			lift.setLiftPosition(LiftPosition.BOTTOM);
 		}
-		
-		if(getLiftSeverity() > )
+	}
+	
+	public void teleopPeriodic(){
+		setMotionProfile(getLiftSeverity());
+		keepLiftSafe();
+	}
+	
+	public void autonomousPeriodic(){
+		keepLiftSafe();
 	}
 }
