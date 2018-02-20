5a8fcef5c81d50b902b8043bbac38e1cc921f910
Billy Huang <hiddendragon66@gmail.com>
2018-02-19 20:08:18 -0600

    Added NavXMonitor to Miyamoto and NotVlad

--- src/NotVlad/Miyamoto.java
+++ src/NotVlad/Miyamoto.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.kauailabs.navx.frc.AHRS;
-import com.kauailabs.navx.frc.AHRS.SerialDataType;
 
 import NotVlad.components.Climber;
 import NotVlad.components.Intake;
@@ -17,10 +16,9 @@ import drive.TwoStickDrive;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SPI.Port;
 import input.EncoderMonitor;
+import input.NavXMonitor;
 import input.SensorController;
 import physicalOutput.TurnController;
 import physicalOutput.motors.ChainMotor;
@@ -50,22 +48,22 @@ public class Miyamoto extends RobotDefinitionBase {
 		_properties = new HashMap<String, String>();
 
 		// Old Motor Pins
-		 _properties.put("FLMOTORPIN", "3");
-		 _properties.put("BLMOTORPIN", "2");
-		 _properties.put("FRMOTORPIN", "1");
-		 _properties.put("BRMOTORPIN", "0");
-		 _properties.put("CLIMBMOTORPIN", "4");
-		 _properties.put("INTAKELEFTPIN", "5");
-		 _properties.put("INTAKERIGHTPIN", "6");
+		_properties.put("FLMOTORPIN", "3");
+		_properties.put("BLMOTORPIN", "2");
+		_properties.put("FRMOTORPIN", "1");
+		_properties.put("BRMOTORPIN", "0");
+		_properties.put("CLIMBMOTORPIN", "4");
+		_properties.put("INTAKELEFTPIN", "5");
+		_properties.put("INTAKERIGHTPIN", "6");
 
 		// New Motor Pins
-//		_properties.put("FLMOTORPIN", "0");
-//		_properties.put("BLMOTORPIN", "1");
-//		_properties.put("FRMOTORPIN", "3");
-//		_properties.put("BRMOTORPIN", "2");
-//		_properties.put("CLIMBMOTORPIN", "4");
-//		_properties.put("INTAKELEFTPIN", "5");
-//		_properties.put("INTAKERIGHTPIN", "6");
+		// _properties.put("FLMOTORPIN", "0");
+		// _properties.put("BLMOTORPIN", "1");
+		// _properties.put("FRMOTORPIN", "3");
+		// _properties.put("BRMOTORPIN", "2");
+		// _properties.put("CLIMBMOTORPIN", "4");
+		// _properties.put("INTAKELEFTPIN", "5");
+		// _properties.put("INTAKERIGHTPIN", "6");
 	}
 
 	/***
@@ -74,25 +72,26 @@ public class Miyamoto extends RobotDefinitionBase {
 	 */
 	public Map<String, IControl> loadControlObjects() {
 
-		SmartWriter.putS("Robot is miyamoto...", "asdf");
+		SmartWriter.putS("Robot is miyamoto...", "2018");
 		// Create map to store public objects
 		Map<String, IControl> iControlMap = super.loadControlObjects();
 
 		Global.controllers = new MiyamotoControl();
-		
+
 		CameraServer.getInstance().startAutomaticCapture();
+		// Create sensor map
+		SensorController sensorController = SensorController.getInstance();
+		// Create the NavX
+		AHRS navX = new AHRS(Port.kMXP);
+		sensorController.registerSensor("NAVX", navX);
+		NavXMonitor navXMonitor = new NavXMonitor();
+		navXMonitor.add("NAVX", navX);
 
 		// Encoder stuff
 		Encoder encoder0 = new Encoder(0, 1, false); // Right
 		Encoder encoder1 = new Encoder(2, 3, true); // Left
 		encoder0.setDistancePerPulse(0.05318);
 		encoder1.setDistancePerPulse(0.05321);
-
-		SensorController sensorController = SensorController.getInstance();
-		AHRS navX = new AHRS(Port.kMXP);
-		System.out.println(navX == null);
-		sensorController.registerSensor("NAVX", navX);
-
 		EncoderMonitor encoderMonitor = new EncoderMonitor();
 		encoderMonitor.add("ENCODER0", encoder0);
 		encoderMonitor.add("ENCODER1", encoder1);
