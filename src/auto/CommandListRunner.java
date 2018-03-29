5de62f53b324e64a7b190bb56711648fb54778f1
leinad2018 <leinad2018@gmail.com>
2018-03-28 19:00:33 -0500

    Finished bad version of logging

--- src/auto/CommandListRunner.java
+++ src/auto/CommandListRunner.java
@@ -1,12 +1,17 @@
 package auto;
 
 import auto.commands.EmptyCommand;
+import comms.FileLoader;
+import edu.wpi.first.wpilibj.Encoder;
+import input.SensorController;
 
 public class CommandListRunner {
 	public int commandNum;
 	private int prevCommandNum;
 	private CommandList commands;
 	private ICommand curCommand;
+	private SensorController sensors;
+	private long commandTime;
 
 	/**
 	 * Constructor for CommandListRunner<br>
@@ -17,6 +22,7 @@ public class CommandListRunner {
 	 *            Name of the robot for unique commands
 	 */
 	public CommandListRunner(CommandList xCommands) {
+		sensors = SensorController.getInstance();
 		commands = xCommands;
 		init();
 	}
@@ -26,6 +32,7 @@ public class CommandListRunner {
 	 */
 	public void init() {
 		commandNum = 0;
+		commandTime = 0;
 		prevCommandNum = -1;
 	}
 
@@ -41,10 +48,16 @@ public class CommandListRunner {
 		if (prevCommandNum != commandNum) {
 			prevCommandNum = commandNum;
 			curCommand.init();
+			commandTime = System.currentTimeMillis();
 		}
 		if (curCommand.run()) {
 			curCommand.stop();
 			commandNum++;
+			long logTime = System.currentTimeMillis();
+			String fileName = "/RobotLog" + logTime;
+			String contents = "Encoder 0: " + ((Encoder)sensors.getSensor("ENCODER0")).get() + "/nEncoder 1: " + ((Encoder)sensors.getSensor("ENCODER1")) + "/nTime to run: " + (logTime-commandTime);
+			FileLoader.writeToFile(fileName, contents);
+			FileLoader.writeToFile(fileName, "Logging took: " + (System.currentTimeMillis()-logTime));
 		}
 		return false;
 	}
