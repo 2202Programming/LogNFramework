a162fd70477635a567c7c1c2cd5c8230f91122ea
leinad2018 <leinad2018@gmail.com>
2018-03-29 15:04:27 -0500

    Created better logging system

--- src/comms/LogWriter.java
+++ src/comms/LogWriter.java
@@ -6,21 +6,22 @@ import java.util.Map;
 public class LogWriter {
 	private static Map<String,ILoggable> loggables = new HashMap<>();
 	
+	/**
+	 * Registers a log rule in the Map that can be ran at any time in the program
+	 * @param name the name of the log rule
+	 * @param logRule the behavior this log will have
+	 */
 	public static void registerLoggable(String name, ILoggable logRule){
 		loggables.put(name, logRule);
 	}
 	
 	public static boolean runLog(String name){
-		
-	}
-}
-
-class LogItem{
-	public String logFile;
-	public ILoggable log;
-	
-	public LogItem(String logFileName, ILoggable logRule){
-		logFile = logFileName;
-		log = logRule;
+		ILoggable item = loggables.get(name);
+		if(item == null){
+			return false;
+		}else{
+			FileLoader.writeToFile(item.getLogFileName(), item.getLogData());
+			return true;
+		}
 	}
 }
