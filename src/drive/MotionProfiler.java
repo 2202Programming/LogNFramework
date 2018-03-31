d87824c49d82d6157ede93681527cf38ae02079b
leinad2018 <leinad2018@gmail.com>
2018-03-31 13:36:07 -0500

    Added utility methods to motion profiling
    We should change drive to use these methods instead of having weird interfaces

--- src/drive/MotionProfiler.java
+++ src/drive/MotionProfiler.java
@@ -1,5 +1,7 @@
 package drive;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 import robot.Global;
 import robot.IControl;
 import robotDefinitions.controls.MotionProfileableController;
@@ -52,6 +54,35 @@ public class MotionProfiler extends IControl {
 	 */
 	public MotionProfile getCurrentProfile(){
 		return profiles[index];
+	}
+	
+	/**
+	 * Returns a new set value given a current set value and the last set value
+	 * @param lastValue the previous set value
+	 * @param setValue the target value
+	 * @return a new value in the direction of setValue that is no farther from the lastValue than the max acceleration of the current profile
+	 */
+	public double capAcceleration(double lastValue, double setValue){
+		double maxAcceleration = profiles[index].getAcceleration();
+		if(Math.abs(setValue-lastValue) > maxAcceleration){
+			return lastValue + Math.signum(setValue-lastValue) * maxAcceleration;
+		}else{
+			return setValue;
+		}
+	}
+	
+	/**
+	 * Makes sure that the setValue is lower than the max velocity
+	 * @param setValue the target value
+	 * @return a new value in the direction of setValue that is less than or equal to the max velocity of the current profile
+	 */
+	public double capVelocity(double setValue){
+		double maxVelocity = profiles[index].getVelocity();
+		if(Math.abs(setValue) > maxVelocity){
+			return Math.signum(setValue) * maxVelocity;
+		}else{
+			return setValue;
+		}
 	}
 
 	public void teleopInit() {
