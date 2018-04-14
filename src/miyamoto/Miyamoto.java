ff79eed36b35ee2b13b5e0a15ecbb128b629f946
Unknown <beastromulus@gmail.com>
2018-04-13 19:11:55 -0500

    Updated Stuff

--- src/miyamoto/Miyamoto.java
+++ src/miyamoto/Miyamoto.java
@@ -13,6 +13,7 @@ import drive.MotionProfile;
 import drive.MotionProfiler;
 import drive.TwoStickDrive;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SPI.Port;
@@ -92,8 +93,8 @@ public class Miyamoto extends RobotDefinitionBase {
 		// Encoder stuff
 		double CPI = 19.04; // use same calibration for left/right.Sensors are very very close. - DPL/TG
 							// 3/28
-		Encoder encoder0 = new Encoder(5, 6, false); // Right Changed from 0,1
-		Encoder encoder1 = new Encoder(7, 8, true); // Left CHanged from 2,3
+		Encoder encoder0 = new Encoder(0, 1, true, EncodingType.k4X); // Right Changed from 0,1
+		Encoder encoder1 = new Encoder(2, 3, true, EncodingType.k4X); // Left Changed from 2,3
 		encoder0.setDistancePerPulse(1.0 / CPI); // was 0.05318);
 		encoder1.setDistancePerPulse(1.0 / CPI); // was 0.05321);
 		EncoderMonitor encoderMonitor = new EncoderMonitor();
@@ -116,7 +117,7 @@ public class Miyamoto extends RobotDefinitionBase {
 		TurnController turnController = new TurnController(left, right);
 		iControlMap.put("TURNCONTROLLER", turnController);
 
-		IDrive drive = new TwoStickDrive(left, right, 1, false);
+		IDrive drive = new TwoStickDrive(left, right, 2, false);
 		iControlMap.put(RobotDefinitionBase.DRIVENAME, drive);
 
 		MotionProfile[] profiles = { new MotionProfile(0.08, 1), new MotionProfile(0.05, 1),
